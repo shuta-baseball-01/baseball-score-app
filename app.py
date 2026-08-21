93535a20f55049a8136aff206534b9f3dcf562a6
shuta-baseball-01 <sidehandthrow.north@gmail.com>
2026-08-21 12:22:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.markdown("""
 
 /* ⑤ スマホではみ出さないように、グラウンド全体を85%に縮小表示する */
 iframe[title="streamlit_image_coordinates.streamlit_image_coordinates"] {
-    transform: scale(0.80);
+    transform: scale(1.0);
 transform-origin: center top !important; /* 🌟 左上(top left)から真ん中(center)に変更！ */
     margin: 0 auto !important; /* 🌟 念のため中央揃えの魔法も追加！ */
     display: block !important;
